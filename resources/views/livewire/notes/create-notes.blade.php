a1e1d2db47c9cfb2bb6375b0a02add054cce3040
bwire40 <emmanuelbwire80@gmail.com>
2025-04-07 12:59:31 +0300

    add note form

--- resources/views/livewire/notes/create-notes.blade.php
+++ resources/views/livewire/notes/create-notes.blade.php
@@ -3,10 +3,25 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    // fields
+
+    public $noteTitle;
+    public $noteBody;
+    public $noteRecipient;
+    public $noteSendDate;
+
+    public function submit()
+    {
+        dd($this->noteTitle);
+    }
 }; ?>
 
 <div>
-
-  <h1>create a note</h1>
+  <form wire:submit='submit' action="" class="space-y-4">
+    <x-input wire:model='noteTitle' label="Note Title" />
+    <x-textarea wire:model='noteBody' label="Your Note" />
+    <x-input wire:model='noteRecipient' label="Recipient" placeholder="yourfriend@email.com" type="email" />
+    <x-input wire:model='noteSendDate' label="Send Date" type="date" />
+    <x-button wire:click='submit' primary right-icon="calendar" spinner>Submit</x-button>
+  </form>
 </div>
